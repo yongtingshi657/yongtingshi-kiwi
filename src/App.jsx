c65e5038cddf3e90deb6d01ead15b56788592833
Yongting <yongtingshi657@gmail.com>
2025-07-21 22:46:05 -0400

    set up and get pics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import ctdLogo from './assets/icons/mono-blue-logo.svg'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function App() {
+    return (
+      <>
+      <div className='coming-soon'>
+        <h1>CTD Swag</h1>
+        <div style={{ height: 100, width: 100 }}>
+            <img src={ctdLogo} alt="Code the Dream Logo" />
+        </div>
+        <h2>Coming soon</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
+    )
 }
 
 export default App
